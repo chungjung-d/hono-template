f6480305206801b11577b18a34bf9e158f838163
chungjung-d <chungjung.d@gmail.com>
2025-09-29 18:14:24 +0900

    env

--- src/router/v1.ts
+++ src/router/v1.ts
@@ -1,12 +1,10 @@
 import { Env, Hono } from 'hono';
 import authRouter from './v1/auth';
 import userRouter from './v1/usert';
-import characterRouter from './v1/character';
 
 const v1Router = new Hono<Env>()
 
 v1Router.route('/auth', authRouter);
 v1Router.route('/user', userRouter);
-v1Router.route('/character', characterRouter);
 
 export default v1Router
